cb4c97a45d4a6b622a785d8d3a87ef79bd62eaff
khairilaminuddin <khairil1721@gmail.com>
2025-02-05 16:08:18 +0800

    learn route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('todos' , [App\Http\Controllers\TodoController::class, 'index']);
+
+Route::resources(['users' => App\Http\Controllers\UserController::class,]);
